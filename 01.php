48b2765af385b800185d42e6adf1a5eac2dd0a98
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-17 14:53:46 +0800

    first commit

---
+++ 01.php
@@ -0,0 +1,18 @@
+<h1>My first Php page</h1>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/17
+ * Time: 下午1:27
+ */
+
+echo "hello world!";
+
+//这是php单行注释
+
+?>
+
+
+
+
---
+++ 02.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/17
+ * Time: 下午2:27
+ * php变量
+ */
+
+
+$x = 5;
+$y = 6;
+$z = $x + $y;
+
+echo $z;
+
+function myTest(){
+
+}
+?>
