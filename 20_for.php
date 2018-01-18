298663e28846927a79b08de93903f05f0719635a
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-18 15:05:04 +0800

    for

---
+++ 20_for.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/18
+ * Time: 下午3:03
+ */
+for ($i = 1; $i <= 5; $i++) {
+    echo "The number is " . $i . "<br>";
+}
+echo "<hr>";
+
+$x = array("one", "two", "three");
+foreach ($x as $value) {
+    echo $value . "<br>";
+}
+echo "<hr>";
