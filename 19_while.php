28c42a453399d063d2210caf2ed2ad2617ac9ee3
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-18 15:03:45 +0800

    while

---
+++ 19_while.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/18
+ * Time: 下午3:02
+ */
+$i = 1;
+while ($i <= 5) {
+    echo "The number is " . $i . "<br>";
+    $i++;
+}
+echo "<hr>";
+
+$i = 1;
+do {
+    $i++;
+    echo "The number is " . $i . "<br>";
+} while ($i <= 5);
+
+
+
+
