91bf269821b47e0ef85671339dc27cb99eddccf2
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-21 15:45:03 +0800

    update

---
+++ 25_表单/05_checkbox 复选框/checkbox.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/21
+ * Time: 下午3:44
+ */
+
+$q = isset($_POST['q']) ? $_POST['q'] : '';
+if (is_array($q)) {
+    $sites = array(
+        'RUNOOB' => '菜鸟教程: http://www.runoob.com',
+        'GOOGLE' => 'Google 搜索: http://www.google.com',
+        'TAOBAO' => '淘宝: http://www.taobao.com',
+    );
+    foreach ($q as $val) {
+        // PHP_EOL 为常量，用于换行
+        echo $sites[$val] . "<br>";
+    }
+
+} else {
+    ?>
+  <form action="" method="post">
+    <input type="checkbox" name="q[]" value="RUNOOB"> Runoob<br>
+    <input type="checkbox" name="q[]" value="GOOGLE"> Google<br>
+    <input type="checkbox" name="q[]" value="TAOBAO"> Taobao<br>
+    <input type="submit" value="提交">
+  </form>
+    <?php
+}
+
+?>
