fd397d3dce7e4091b1d7164d376cab2ae862416a
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-22 15:18:34 +0800

    PHP 异常处理

---
+++ 39_异常处理/02.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/22
+ * Time: 下午3:04
+ */
+//适当的处理异常代码应该包括：
+//Try - 使用异常的函数应该位于 "try" 代码块内。如果没有触发异常，则代码将照常继续执行。但是如果异常被触发，会抛出一个异常。
+//Throw - 里规定如何触发异常。每一个 "throw" 必须对应至少一个 "catch"。
+//Catch - "catch" 代码块会捕获异常，并创建一个包含异常信息的对象。
+
+function checkNum($number)
+{
+    if ($number > 1) {
+        throw new Exception("变量值必须小于等于 1");
+    }
+    return true;
+}
+
+// 在 try 块 触发异常
+try {
+    checkNum(2);
+    // 如果抛出异常，以下文本不会输出
+    echo '如果输出该内容，说明 $number 变量';
+} // 捕获异常
+catch (Exception $e) {
+    echo 'Message: ' . $e->getMessage();
+}
+
+echo "<br>";
+try {
+    throw new Exception("test error");
+} catch (Exception $e) {
+    echo 'Message: ' . $e->getMessage();
+}
+?>
---
+++ 39_异常处理/03.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/22
+ * Time: 下午3:07
+ */
+//创建一个自定义的 Exception 类
+//创建自定义的异常处理程序非常简单。我们简单地创建了一个专门的类，当 PHP 中发生异常时，可调用其函数。该类必须是 exception 类的一个扩展。
+//这个自定义的 customException 类继承了 PHP 的 exception 类的所有属性，您可向其添加自定义的函数。
+//我们开始创建 customException 类：
+
+class customException extends Exception
+{
+    public function errorMessage()
+    {
+        // 错误信息
+        $errorMsg = '错误行号 ' . $this->getLine() . ' in ' . $this->getFile()
+            . ': <b>' . $this->getMessage() . '</b> 不是一个合法的 E-Mail 地址';
+        return $errorMsg;
+    }
+}
+
+$email = "someone@example...com";
+
+try {
+    // 检测邮箱
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) === FALSE) {
+        // 如果是个不合法的邮箱地址，抛出异常
+        throw new customException($email);
+    }
+} catch (customException $e) {
+//display custom message
+    echo $e->errorMessage();
+}
+
+?>
