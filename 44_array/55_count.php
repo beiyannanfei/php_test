404b4098c8d50e3e4613d3d93dbe1c06a1adbd65
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 18:11:26 +0800

    PHP count() 函数

---
+++ 44_array/55_count.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午6:06
+ */
+
+//count() 函数返回数组中元素的数目。
+$cars = array("Volvo", "BMW", "Toyota");
+echo count($cars);  //3
+
+?>
---
+++ 44_array/56_current.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午6:07
+ */
+
+$people = array("Peter", "Joe", "Glenn", "Cleveland");
+//current() 函数返回数组中的当前元素的值。
+echo current($people) . "<br>"; //Peter
+
+?>
