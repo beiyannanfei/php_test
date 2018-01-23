21f790111347675c124a81cc6bc26e39595be2fb
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 18:15:02 +0800

    PHP end() 函数

---
+++ 44_array/58_end.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午6:14
+ */
+
+$people = array("Peter", "Joe", "Glenn", "Cleveland");
+
+//end() 函数将内部指针指向数组中的最后一个元素，并输出。
+echo end($people);  //Cleveland
+
+?>
