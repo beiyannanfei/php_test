fd0c3525cba3b72e5af7ff47e552931c27454647
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 17:32:20 +0800

    PHP array_search() 函数

---
+++ 44_array/36_array_search.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午5:31
+ */
+
+$a = array("a" => "red", "b" => "green", "c" => "blue");
+//array_search() 函数在数组中搜索某个键值，并返回对应的键名。
+echo array_search("red", $a);   //a
+
+?>
