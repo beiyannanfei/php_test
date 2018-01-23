fb94cec3c2c4205be8c37c18cc0843e453fb260a
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 18:34:42 +0800

    PHP range() 函数

---
+++ 44_array/70_range.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午6:32
+ */
+
+//range() 函数创建一个包含指定范围的元素的数组
+$number = range(0, 5);
+print_r($number);   //Array ( [0] => 0 [1] => 1 [2] => 2 [3] => 3 [4] => 4 [5] => 5 )
+
+?>
