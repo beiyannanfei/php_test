0d79bd3d88d4e6c29e58a2424c874a80d8420918
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 18:36:06 +0800

    PHP range() 函数

--- 44_array/70_range.php
+++ 44_array/70_range.php
@@ -9,5 +9,8 @@
 //range() 函数创建一个包含指定范围的元素的数组
 $number = range(0, 5);
 print_r($number);   //Array ( [0] => 0 [1] => 1 [2] => 2 [3] => 3 [4] => 4 [5] => 5 )
+echo "<br>";
 
+$letter = range("a", "d");
+print_r($letter);   //Array ( [0] => a [1] => b [2] => c [3] => d )
 ?>
