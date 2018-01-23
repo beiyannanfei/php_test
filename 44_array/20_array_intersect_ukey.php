e3934a2922cbac6698e91a0f667b865935fe71b3
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 16:41:24 +0800

    PHP array_intersect_ukey() 函数

--- 44_array/20_array_intersect_ukey.php
+++ 44_array/20_array_intersect_ukey.php
@@ -23,6 +23,6 @@ $a3 = array("e" => "purple", "f" => "white", "a" => "gold");
 //该函数比较两个（或更多个）数组的键名，并返回一个交集数组，该数组包括了所有在被比较的数组（array1）中，
 //同时也在任何其他参数数组（array2 或 array3 等等）中的键名。
 $result = array_intersect_ukey($a1, $a2, $a3, "myfunction");
-print_r($result);
+print_r($result);   //Array ( [a] => red )
 
 ?>
