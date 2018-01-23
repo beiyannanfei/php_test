d47cb01939b56bbba04bd6e27dc16ffbcad2bc32
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 18:40:19 +0800

    PHP sort() 函数

---
+++ 44_array/75_sort.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午6:39
+ */
+
+$cars = array("Volvo", "BMW", "Toyota");
+//sort() 函数对数值数组进行降序排序。
+sort($cars);
+print_r($cars); //Array ( [0] => BMW [1] => Toyota [2] => Volvo )
+?>
