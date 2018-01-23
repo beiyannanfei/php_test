2cf58f061b51fc7f37076747a9156ab9b42d31de
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 18:39:17 +0800

    PHP sizeof() 函数

---
+++ 44_array/74_sizeof.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午6:38
+ */
+
+//sizeof() 函数返回数组中元素的数目。
+//sizeof() 函数是 count() 函数的别名。
+$cars = array("Volvo", "BMW", "Toyota");
+echo sizeof($cars);     //3
+
+?>
