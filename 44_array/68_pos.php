55079aa89dedd613ab97dca59b03729180cbd465
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 18:30:25 +0800

    PHP pos() 函数

---
+++ 44_array/68_pos.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午6:29
+ */
+
+$people = array("Peter", "Joe", "Glenn", "Cleveland");
+//pos() 函数返回数组中的当前元素的值。
+//该函数是 current() 函数的别名。
+echo pos($people) . "<br>"; //Peter
+
+?>
