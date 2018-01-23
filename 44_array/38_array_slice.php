d606425c63c4e4c17e7692de000f6e5727360d05
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 17:39:26 +0800

    PHP array_slice() 函数

---
+++ 44_array/38_array_slice.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午5:38
+ */
+
+$a = array("red", "green", "blue", "yellow", "brown");
+//array_slice() 函数返回数组中的选定部分。
+print_r(array_slice($a, 2));    //Array ( [0] => blue [1] => yellow [2] => brown )
+
+?>
