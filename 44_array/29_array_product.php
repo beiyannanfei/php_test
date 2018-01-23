419a44ae4093af54a0671508560237001a4c9cdc
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 17:21:49 +0800

    PHP array_product() 函数

---
+++ 44_array/29_array_product.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午5:20
+ */
+
+$a = array(5, 5, 3, 2, 7);
+//array_product() 函数计算并返回数组的乘积。
+echo(array_product($a));    //1050
+
+?>
