251e4c024374fed90d23ec947e83bdcf50f7bf87
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 15:56:46 +0800

    PHP array_fill() 函数

---
+++ 44_array/12_array_fill.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午3:56
+ */
+
+$a1 = array_fill(3, 4, "blue");
+print_r($a1);   //Array ( [3] => blue [4] => blue [5] => blue [6] => blue )
+
+?>
