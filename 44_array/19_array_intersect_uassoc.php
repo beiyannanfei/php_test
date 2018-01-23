f7359beabc502ddd318f830867d4b32d8e87e3fc
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 16:39:51 +0800

    PHP array_intersect_uassoc() 函数

---
+++ 44_array/19_array_intersect_uassoc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午4:39
+ */
+
+function myfunction($a, $b)
+{
+    if ($a === $b) {
+        return 0;
+    }
+    return ($a > $b) ? 1 : -1;
+}
+
+$a1 = array("a" => "red", "b" => "green", "c" => "blue");
+$a2 = array("d" => "red", "b" => "green", "e" => "blue");
+
+//array_intersect_uassoc() 函数用于比较两个（或更多个）数组的键名和键值 ，并返回交集。
+//注释：该函数使用用户自定义函数比较键名！
+//该函数比较两个（或更多个）数组的键名和键值，并返回一个交集数组，该数组包括了所有在被比较的数组（array1）中，
+//同时也在任何其他参数数组（array2 或 array3 等等）中的键名和键值。
+$result = array_intersect_uassoc($a1, $a2, "myfunction");
+print_r($result);
+
+
+?>
