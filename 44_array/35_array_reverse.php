783f4d8dc1dd915e11c1128f57d63d52a9f30325
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 17:31:25 +0800

    PHP array_reverse() 函数

---
+++ 44_array/35_array_reverse.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午5:30
+ */
+
+$a = array("a" => "Volvo", "b" => "BMW", "c" => "Toyota");
+//array_reverse() 函数返回翻转顺序的数组。
+print_r(array_reverse($a)); //Array ( [c] => Toyota [b] => BMW [a] => Volvo )
+
+?>
