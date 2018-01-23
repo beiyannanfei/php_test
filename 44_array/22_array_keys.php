9cc0877628463c6b1a85dd7cba521e248b181e3e
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 16:43:36 +0800

    PHP array_keys() 函数

---
+++ 44_array/22_array_keys.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午4:43
+ */
+//array_keys() 函数返回包含数组中所有键名的一个新数组。
+$a = array("Volvo" => "XC90", "BMW" => "X5", "Toyota" => "Highlander");
+print_r(array_keys($a));    //Array ( [0] => Volvo [1] => BMW [2] => Toyota )
+
+?>
