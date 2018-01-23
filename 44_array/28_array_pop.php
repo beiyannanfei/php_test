811a208d93163ed286839808a24b34de75a8cd02
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-23 17:04:00 +0800

    PHP array_pop() 函数

---
+++ 44_array/28_array_pop.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/23
+ * Time: 下午5:01
+ */
+
+$a = array("red", "green", "blue");
+//array_pop() 函数删除数组中的最后一个元素。
+array_pop($a);
+print_r($a);    //Array ( [0] => red [1] => green )
+
+
+?>
