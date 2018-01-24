7231a30b9d9538430faf50fea278a479e0fb17ef
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-24 18:12:30 +0800

    PHP date_default_timezone_get() 函数

---
+++ 46_dateTime/06_date_default_timezone_get.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/24
+ * Time: 下午6:11
+ */
+
+//date_default_timezone_get() 函数返回脚本中所有日期/时间函数使用的默认时区。
+echo date_default_timezone_get();   //Europe/Zurich
+
+?>
