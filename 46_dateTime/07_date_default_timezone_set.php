9bfd4148cf71dcd605c7f94bb94e144f560a44bf
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-24 18:13:16 +0800

    PHP date_default_timezone_set() 函数

---
+++ 46_dateTime/07_date_default_timezone_set.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/24
+ * Time: 下午6:12
+ */
+
+//date_default_timezone_set() 函数设置脚本中所有日期/时间函数使用的默认时区。
+date_default_timezone_set("Asia/Shanghai");
+echo date_default_timezone_get();   //Asia/Shanghai
+
+?>
