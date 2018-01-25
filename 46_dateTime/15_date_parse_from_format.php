eaa05765791cd55769b31ddb76769da8346e5c4b
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 11:37:53 +0800

    PHP date_parse_from_format() 函数

---
+++ 46_dateTime/15_date_parse_from_format.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/25
+ * Time: 上午11:35
+ */
+
+echo "<pre>";
+print_r(date_parse_from_format("mmddyyyy", "05122013"));
+echo "</pre>";
+
+?>
