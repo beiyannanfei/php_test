b2550d427b1055bf486b88588d013c49e16242ec
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 14:58:10 +0800

    PHP getcwd() 函数

---
+++ 47_Directory/05_getcwd.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/25
+ * Time: 下午2:57
+ */
+
+//getchwd() 函数返回当前工作目录。
+echo getcwd();  ///Users/sensoro/bynf/PHP/mytest/47_Directory
+?>
