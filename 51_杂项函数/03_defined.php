bde6ab8c658d37cc8d07c62052e2605cade4e530
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 17:50:30 +0800

    PHP defined() 函数

---
+++ 51_杂项函数/03_defined.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/25
+ * Time: 下午5:49
+ */
+
+define("GREETING", "Hello you! How are you today?");
+var_dump(defined("GREETING"));
+var_dump(defined("GREETING1"));
+
+?>
