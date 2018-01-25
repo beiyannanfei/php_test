838f4b23f795110eaa7fb3dc13d877c91cccb756
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 17:47:01 +0800

    PHP constant() 函数

---
+++ 51_杂项函数/01_constant.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/25
+ * Time: 下午5:45
+ */
+
+define("GREETING", "Hello you! How are you today?");
+
+echo constant("GREETING") . "<br>";
+echo GREETING;
+?>
