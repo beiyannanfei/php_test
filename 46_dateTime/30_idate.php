1097536000a90960025c6de553f3869f6d54ce7f
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 14:31:50 +0800

    PHP idate() 函数

---
+++ 46_dateTime/30_idate.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/25
+ * Time: 下午2:31
+ */
+//idate() 函数格式化本地时间/日期为整数。
+echo idate("B") . "<br>";
+echo idate("d") . "<br>";
+echo idate("h") . "<br>";
+echo idate("H") . "<br>";
+echo idate("i") . "<br>";
+echo idate("I") . "<br>";
+echo idate("L") . "<br>";
+echo idate("m") . "<br>";
+echo idate("s") . "<br>";
+echo idate("t") . "<br>";
+echo idate("U") . "<br>";
+echo idate("w") . "<br>";
+echo idate("W") . "<br>";
+echo idate("y") . "<br>";
+echo idate("Y") . "<br>";
+echo idate("z") . "<br>";
+echo idate("Z") . "<br>";
+?>
