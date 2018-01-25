58448e01fe467312e3d2050f5311e50a8a327831
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 11:40:36 +0800

    PHP date_parse() 函数

--- 46_dateTime/16_date_parse.php
+++ 46_dateTime/16_date_parse.php
@@ -6,7 +6,32 @@
  * Time: 上午11:38
  */
 
-
+echo "<pre>";
 print_r(date_parse("2013-05-01 12:30:45.5"));
+echo "</pre>";
+
+/*
+Array
+(
+    [year] => 2013
+    [month] => 5
+    [day] => 1
+    [hour] => 12
+    [minute] => 30
+    [second] => 45
+    [fraction] => 0.5
+    [warning_count] => 0
+    [warnings] => Array
+        (
+        )
+
+    [error_count] => 0
+    [errors] => Array
+        (
+        )
+
+    [is_localtime] =>
+)
+*/
 
 ?>
