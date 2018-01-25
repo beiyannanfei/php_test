79400848dd1ee90b66250e29f2a35c3650a68a23
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 14:40:08 +0800

    PHP time() 函数

---
+++ 46_dateTime/37_time.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/25
+ * Time: 下午2:39
+ */
+//time() 函数返回自 Unix 纪元（January 1 1970 00:00:00 GMT）起的当前时间的秒数。
+$t = time();
+echo($t . "<br>");
+echo(date("Y-m-d", $t));
+
+?>
