09af94c73df0cd1faace32bdb835ffc41d2604a6
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-25 11:50:16 +0800

    update

---
+++ 46_dateTime/20_date_timestamp_get.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/25
+ * Time: 上午11:49
+ */
+
+//date_timestamp_get() 函数返回 Unix 时间戳。
+$date = date_create();
+echo date_timestamp_get($date); //1516852194
+
+?>
