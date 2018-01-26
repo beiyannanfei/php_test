2765ab59078cef870e57ab09d4594542943a6c09
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 14:46:40 +0800

    PHP ord() 函数

---
+++ 52_String/19_ord.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午2:46
+ */
+
+//ord() 函数返回字符串中第一个字符的 ASCII 值。
+echo ord("h") . "<br>";         //104
+echo ord("hello") . "<br>";     //104
+?>
