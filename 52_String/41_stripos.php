5fbbcb2757cd477cf287dd1f738454d6027028d2
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:41:58 +0800

    PHP stripos() 函数

---
+++ 52_String/41_stripos.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:41
+ */
+
+//stripos() 函数查找字符串在另一字符串中第一次出现的位置（不区分大小写）。
+
+
+echo stripos("I love php, I love php too!", "PHP"); //7
+?>
