7d9958fc18066e19e37c9fe11962b8018ac7ed00
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 11:48:12 +0800

    PHP hex2bin() 函数

---
+++ 52_String/10_hex2bin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 上午11:47
+ */
+
+//hex2bin() 函数把十六进制值的字符串转换为 ASCII 字符。
+echo hex2bin("48656c6c6f20576f726c6421");       //out: Hello World!
+?>
