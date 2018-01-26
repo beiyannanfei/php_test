f4616e424e58152d548ec6e55671555cb71b9ff2
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:43:15 +0800

    PHP stristr() 函数

---
+++ 52_String/42_stristr.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:42
+ */
+
+//stristr() 函数搜索字符串在另一字符串中的第一次出现
+//返回值：	返回字符串的剩余部分（从匹配点）。如果未找到所搜索的字符串，则返回 FALSE。
+
+echo stristr("Hello world!", "WORLD");  //world!
+?>
