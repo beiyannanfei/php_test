16c833825049b1235e898b6164f3c69d213add8b
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:26:10 +0800

    PHP strchr() 函数

---
+++ 52_String/38_strchr.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:22
+ */
+
+
+
+//查找 "world" 在 "Hello world!" 中的第一次出现，并返回字符串的其余部分：
+echo strchr("Hello world!abcdef", "world"); //world!abcdef
+?>
