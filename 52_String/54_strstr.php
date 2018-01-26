ac7597c5b3086580e5ecd07a1ca3d2123e3a1abe
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 18:03:11 +0800

    PHP strstr() 函数

---
+++ 52_String/54_strstr.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午6:02
+ */
+//strstr() 函数搜索字符串在另一字符串中的第一次出现。
+
+//查找 "world" 在 "Hello world!" 中的第一次出现，并返回字符串的剩余部分：
+echo strstr("Hello world!", "world");   //world!
+?>
