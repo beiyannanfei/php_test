76db08fe786d4f1f7baedb4303a8614dff36871a
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:58:19 +0800

    PHP strspn() 函数

---
+++ 52_String/53_strspn.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:57
+ */
+
+//strspn() 函数返回在字符串中包含 charlist 参数中指定的字符数目。
+echo strspn("Hello world!", "kHlleo");  //5
+?>
