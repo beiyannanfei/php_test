f3c55754dcf40e10eb534f14bb405e0cea330f57
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:57:18 +0800

    PHP strrpos() 函数

---
+++ 52_String/52_strrpos.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:56
+ */
+
+//strrpos() 函数查找字符串在另一字符串中最后一次出现的位置（区分大小写）。
+//注释：strrpos() 函数是区分大小写的。
+
+echo strrpos("I love pHp, I love php too!","pHp");  //7
+?>
