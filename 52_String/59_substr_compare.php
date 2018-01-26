bedac28462f2b5a686707b948368dbbe250211f2
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 18:11:32 +0800

    PHP substr_compare() 函数

---
+++ 52_String/59_substr_compare.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午6:10
+ */
+//substr_compare() 函数从指定的开始位置比较两个字符串。
+//提示：该函数是二进制安全和选择性区分大小写。
+
+echo substr_compare("Hello world", "Hello world", 0);   //0
+?>
