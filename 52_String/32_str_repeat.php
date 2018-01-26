fc6ae55857d3da9a37c4f83ddf7bd98c1052fed6
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 16:55:57 +0800

    PHP str_repeat() 函数

---
+++ 52_String/32_str_repeat.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午4:55
+ */
+
+//str_repeat() 函数把字符串重复指定的次数。
+echo str_repeat("AB", 13);  //ABABABABABABABABABABABABAB
+?>
