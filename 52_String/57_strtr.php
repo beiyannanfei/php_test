9f79e81d8f08a0023c4b786e373df566c2bf3188
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 18:07:39 +0800

    PHP strtr() 函数

---
+++ 52_String/57_strtr.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午6:06
+ */
+
+//strtr() 函数转换字符串中特定的字符。
+//注释：如果 from 和 to 参数的长度不同，则格式化为最短的长度。
+
+//把字符串中的字符 "ia" 替换成 "eo"：
+echo strtr("Hilla Warld", "ia", "eo");  //Hello World
+?>
