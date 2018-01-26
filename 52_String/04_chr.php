efa8d757a1267b7fee147fa19dfa92a0f41f3445
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 11:06:45 +0800

    PHP chr() 函数

---
+++ 52_String/04_chr.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 上午11:06
+ */
+
+echo chr(52) . "<br>"; // Decimal value
+echo chr(052) . "<br>"; // Octal value
+echo chr(0x52) . "<br>"; // Hex value
+/*
+out:
+4
+*
+R
+*/
+?>
