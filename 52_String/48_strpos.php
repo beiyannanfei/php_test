e1fc0f44604c0c930aa3521399e1401f307b0af8
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:52:53 +0800

    PHP strpos() 函数

---
+++ 52_String/48_strpos.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:52
+ */
+
+//strpos() f函数查找字符串在另一字符串中第一次出现的位置（区分大小写）。
+echo strpos("I love php, I love php too!", "php");  //7
+?>
