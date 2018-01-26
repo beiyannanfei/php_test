983dce2d441caa056ea1f556f57784ba817ba83a
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:56:18 +0800

    PHP strripos() 函数

---
+++ 52_String/51_strripos.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:55
+ */
+
+//strripos() 函数查找字符串在另一字符串中最后一次出现的位置（不区分大小写）。
+//注释：strripos() 函数是不区分大小写的。
+
+echo strripos("I love php, I love php too!", "PHP");    //19
+?>
