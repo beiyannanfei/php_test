0de010682b075534577c4331e2ce91f2baf5dfbd
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 18:08:32 +0800

    PHP substr() 函数

---
+++ 52_String/58_substr.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午6:07
+ */
+
+//substr() 函数返回字符串的一部分。
+//注释：如果 start 参数是负数且 length 小于或等于 start，则 length 为 0。
+
+echo substr("Hello world", 6);  //world
+?>
