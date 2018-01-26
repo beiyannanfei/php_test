b612666b08af4860bd88fcd7a3692d48a546a147
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 10:23:36 +0800

    PHP sleep() 函数

---
+++ 51_杂项函数/08_sleep.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 上午10:22
+ */
+
+echo date('h:i:s') . "<br>";
+
+//sleep for 5 seconds
+sleep(5);   //sleep() 函数延迟执行当前脚本若干秒。
+
+//start again
+echo date('h:i:s');
+
+?>
