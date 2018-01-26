df75e9b49d5c601bd92449d22971c1f33c97a664
wangyanqiang <wangyanqiang@sensoro.com>
2018-01-26 17:54:17 +0800

    PHP strrchr() 函数

---
+++ 52_String/49_strrchr.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 18/1/26
+ * Time: 下午5:53
+ */
+
+//strrchr() 函数查找字符串在另一个字符串中最后一次出现的位置，并返回从该位置到字符串结尾的所有字符。
+echo strrchr("Hello worlda worldb worldc", "world");    //worldc
+?>
