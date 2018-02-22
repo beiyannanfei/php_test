40432e1a83ca9ef1a582f92579f1658b1837b6e3
wangyanqiang <wangyanqiang@sensoro.com>
2018-02-22 17:13:06 +0800

    update

--- 46_dateTime/36_strtotime.php
+++ 46_dateTime/36_strtotime.php
@@ -13,6 +13,8 @@ echo(strtotime("+5 hours") . "<br>");
 echo(strtotime("+1 week") . "<br>");
 echo(strtotime("+1 week 3 days 7 hours 5 seconds") . "<br>");
 echo(strtotime("next Monday") . "<br>");
-echo(strtotime("last Sunday"));
+echo(strtotime("last Sunday") . "<br>");
+//date_default_timezone_set("Asia/Shanghai");
+echo(strtotime("2018-02-22 16:47:18"))  //1519289238(设置时区)  1519314438(未设置)
 
 ?>
--- 46_dateTime/37_time.php
+++ 46_dateTime/37_time.php
@@ -8,6 +8,6 @@
 //time() 函数返回自 Unix 纪元（January 1 1970 00:00:00 GMT）起的当前时间的秒数。
 $t = time();
 echo($t . "<br>");
-echo(date("Y-m-d", $t));
+echo(date("Y-m-d H:i:s", $t));
 
 ?>
