cd0b76314855f61bf2050532ac7de639936421ab
wyq <18810776836@163.com>
2018-03-14 11:54:10 +0800

    update

---
+++ 0_实用总结/01_时间戳(microtime).php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyq
+ * Date: 2018/3/14
+ * Time: 上午11:50
+ */
+//返回当前 Unix 时间戳的微秒数
+
+$timestamp = microtime();
+var_dump($timestamp);   //string(21) "0.87977600 1520999604"
+echo "<br>";
+//当设置为 TRUE 时，规定函数应该返回浮点数，否则返回字符串。默认为 FALSE
+$timestamp = microtime(true);
+var_dump($timestamp); //float(1520999604.8798)
+
+
+?>
