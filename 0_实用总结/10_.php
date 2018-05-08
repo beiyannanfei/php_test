8d28abe1c54f8bfc71cb497397ef30e75633294b
wyq <18810776836@163.com>
2018-05-08 18:29:17 +0800

    update

--- 0_实用总结/10_.php
+++ 0_实用总结/10_.php
@@ -6,5 +6,18 @@
  * Time: 上午10:48
  */
 
+/**
+ * @param $arr
+ */
+function test(&$arr)
+{
+    $arr['a'] = 10;
+}
 
-?>
+$arr = array('b' => 20);
+
+var_dump($arr);
+echo '<br>';
+test($arr);
+var_dump($arr);
+
